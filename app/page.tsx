0a695f2c3d5a337bff7f4c160920156dab1b91d3
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2025-01-15 19:47:16 -0300

    feat: adding new camera

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,12 @@ export default function Home() {
     scene.background = new THREE.Color(0xffffff);
   
     const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(10, 20, -10);
+    camera.position.set(40, 20, -10);
   
     let table;
     let monitor;
+    let chair;
+    let pc;
   
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
@@ -38,35 +40,49 @@ export default function Home() {
   
     const loader = new GLTFLoader();
   
-    loader.load("./model/table.glb", function (gltf: { scene: any; }) {
+    loader.load("./model/table.glb", function (gltf) {
       table = gltf.scene;
       table.position.x = 15;
       scene.add(gltf.scene);
     });
   
-    loader.load("./model/monitor.glb", function (gltf: { scene: any; }) {
+    loader.load("./model/monitor.glb", function (gltf) {
       monitor = gltf.scene;
-      monitor.position.x = -4;
-      monitor.position.y = 3.5;
+      monitor.position.x = 11.5;
+      monitor.position.y = 4.25;
       monitor.position.z = -6;
+      scene.add(gltf.scene);
+    });
+
+    loader.load("./model/chair.glb", function (gltf) {
+      chair = gltf.scene;
+      chair.rotation.y = -1.6;
+      chair.position.x = 16;
+      chair.position.z = -7;
+      scene.add(gltf.scene);
+    });
+
+    loader.load("./model/pc.glb", function (gltf) {
+      pc = gltf.scene;
+      pc.position.y = 4.25;
+      pc.position.x = 12.5;
       scene.add(gltf.scene);
     });
   
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.minDistance = 20;
-    controls.maxDistance = 30;
-    controls.maxPolarAngle = Math.PI / 4;
   
     let startTime: number | null = null;
-    let targetPosition = new THREE.Vector3(-1, 8.75, -5.25);
-    let initialPosition = camera.position.clone();
-    let initialRotation = camera.rotation.clone();
-    let targetRotation = new THREE.Euler(-1.56, 1.22, 1.56);
-    let duration = 1000;
+    const targetPosition = new THREE.Vector3(12, 8.75, -5.25);
+    const initialPosition = camera.position.clone();
+    const initialRotation = camera.rotation.clone();
+    const targetRotation = new THREE.Euler(-1.56, 1.22, 1.56);
+    const duration = 1000;
 
     function animate() {
       requestAnimationFrame(animate);
       controls.update();
+
+      console.log(camera);
   
       if (startTime) {
         let elapsedTime = (performance.now() - startTime) / duration;
@@ -86,12 +102,12 @@ export default function Home() {
     }
   
     setTimeout(() => {
-      startTime = performance.now();
+        startTime = performance.now();
     }, 2000);
 
     setTimeout(() => {
         setIs3d(false);
-    }, 3500);
+    }, 3000);
   
     animate();
     }
